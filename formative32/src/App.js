40859b301a83c4ee184d8698e7ebc5f10096cb22
Logan <lpun004@aucklanduni.ac.nz>
2019-08-22 12:27:31 +1200

    hooked-up to api

--- formative32/src/App.js
+++ formative32/src/App.js
@@ -1,19 +1,28 @@
 import React, {Component} from 'react';
-
+import axios from 'axios'
 import View from './View';
 import Project from './Project';
-
 import './App.css';
 
+// https://github.com/u4us/formative3-2
 
-class  App extends Component {
+var urlPrefix = 'http://localhost:3001/api'
 
+class App extends Component {
   constructor(props){
     super(props)
 
     this.state = {
-      activeView:'home'
-
+      activeView:'home',
+      icecreams: [
+        {
+          id: 1,
+          flavour: 'chocolate',
+          parlour: 'maccas',
+          user: 'henry',
+          rating: 4,
+        }
+      ],
     };
 
   }
@@ -21,52 +30,58 @@ class  App extends Component {
     this.setState({activeView:view});
   }
   
-
+  getIcecreams = () =>{
+		axios.get(urlPrefix+'/icecreams')
+		.then(res=>{
+			console.log(res);
+			this.setState({icecreams:res.data})
+		})
+  }
   
-
-
+  componentDidMount(){
+    this.getIcecreams();
+  }
 
   render(){
-
-  
     return (
         <div className="app">
-
-			
-      
           <View viewName="home" activeView={this.state.activeView} className="color1" >
-
-          <div className="header "><div className="navbar"><i onClick={() => this.setActiveView('nav')} className="fas fa-bars"></i></div><h2>Rate your Ice Cream</h2><div className="navadd"><div className="addIcecream" onClick={() => this.setActiveView('add-project')} ><img src="add-icecream.png" alt="icecream" class="icecream" /></div></div></div>
-					<div className="main">
+            <div className="header "><div className="navbar"><i onClick={() => this.setActiveView('nav')} className="fas fa-bars"></i></div><h2>Rate your Ice Cream</h2><div className="navadd"><div className="addIcecream" onClick={() => this.setActiveView('add-project')} ><img src="add-icecream.png" alt="icecream" class="icecream" /></div></div></div>
+            <div className="main">
               <h3>Home</h3>
               <p onClick={() => this.setActiveView('about')} >Go to about</p>
-			  <Project/>
-			  <Project/>
-			  <Project/>
+              {
+                this.state.icecreams.map((icecreams)=>{
+                  var icecreamProps = {
+                    ...icecreams,
+                    key: icecreams.id,
+                  }
+                  return(<Project {...icecreamProps}/>)
+                })
+              }
             </div>
-
           </View>
 
           <View viewName="about" activeView={this.state.activeView} className="color2" >
 
-            <div class="header"><i onClick={() => this.setActiveView('nav')} class="fas fa-times"></i></div>
-            <div class="main">
+            <div className="header"><i onClick={() => this.setActiveView('nav')} className="fas fa-times"></i></div>
+            <div className="main">
               About
             </div>
 
           </View>
           <View viewName="login" activeView={this.state.activeView} className="color3" >
 
-            <div class="header"><i onClick={() => this.setActiveView('nav')} class="fas fa-times"></i></div>
-            <div class="main">
+            <div className="header"><i onClick={() => this.setActiveView('nav')} className="fas fa-times"></i></div>
+            <div className="main">
               Login
             </div>
 
           </View>
           <View viewName="nav" activeView={this.state.activeView} className="color5" >
 
-            <div class="header"><i class="fas fa-times" onClick={() => this.setActiveView('home')}></i></div>
-            <div class="main">
+            <div className="header"><i className="fas fa-times" onClick={() => this.setActiveView('home')}></i></div>
+            <div className="main">
 
               <ul className="menu">
                 <li><a onClick={() => this.setActiveView('home')} className="color1" href="#">Home</a></li>
@@ -77,15 +92,9 @@ class  App extends Component {
             </div>
 
           </View>
-
-		  
-
         </div>
     );
   }
-
-
-
 }
 
 export default App;
--- formative32/src/Project.js
+++ formative32/src/Project.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-// https://github.com/u4us/formative3-2
-
-
 class Project extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
 	render(){
 		return(
 			<div className="card project">
 				<img className="card-img-top" src="pic1.jpg" alt="Card image cap" />
 				<div className="card-body">
-				<h5 className="card-title">Review1</h5>
-				<p className="card-text">blahblahjuhee</p>
+				<h5 className="card-title">{this.props.parlour}</h5>
+				<p className="card-text">{this.props.flavour} flavour, rating: {this.props.rating}</p>
+				<p className="card-text">{this.props.user}</p>
 				<p>
 					<i className="fas fa-heart"></i>
 					<i className="fas fa-edit"></i>
 					<i className="fas fa-trash"></i>
 				</p>
-			
 				</div>
 			</div>
 		)
